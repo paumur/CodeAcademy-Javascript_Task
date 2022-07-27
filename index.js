cb85b200e2060b2126b48398d8236468eb1ba348
Paulius Murašovas <pauliusmurasovas@gmail.com>
2022-07-27 21:49:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-let text = document.querySelector("h1");
-const links = document.querySelectorAll("a");
 
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", (e) => {
-    e.preventDefault();
-    text.textContent = `Soon you will be redirected to ${e.target.textContent}`;
-    alert(`You clicked ${e.target.textContent}`);
-    window.location.href = `https://${e.target.textContent}.com`;
-  });
-}
